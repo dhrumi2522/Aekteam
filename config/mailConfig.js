4bde3564c2f87f9237c437656b06b12cdd763112
aekads <aekads.in@gmail.com>
2026-05-20 13:34:37 +0530

    dd
    ddd

--- config/mailConfig.js
+++ config/mailConfig.js
@@ -1,11 +1,11 @@
 const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-         user: 'aekads.otp@gmail.com',
-          pass: "cyns hdye yfwg bezn",
-    },
+  service: 'gmail',
+  auth: {
+    user: 'aekads.otp@gmail.com',
+    pass: 'lwvk otro rxad irbi',
+  },
 });
 
 module.exports = transporter;
