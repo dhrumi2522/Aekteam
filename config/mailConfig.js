f85b025d1096602a7dc9cfd10be009d9050583a2
aekads <aekads.in@gmail.com>
2026-07-14 11:01:17 +0530

    Rotate Gmail password in mailConfig
    Replaced the Gmail account password in config/mailConfig.js for the nodemailer transporter. This updates the 'pass' value used for authentication. Recommend moving credentials to environment variables or a secrets manager to avoid committing secrets to source control.

--- config/mailConfig.js
+++ config/mailConfig.js
@@ -4,7 +4,7 @@ const transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
     user: 'aekads.otp@gmail.com',
-    pass: 'lwvk otro rxad irbi',
+    pass: 'yjya gyzx jnrt vtfu',
   },
 });
 
